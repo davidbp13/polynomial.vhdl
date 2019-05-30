3030448ad8fb758b2bbbb43dfcb46f95530f74a3
davidbp13 <davidbp.13@gmail.com>
2019-05-30 16:49:40 +0200

    yet another solution

--- polynom.vhd
+++ polynom.vhd
@@ -6,6 +6,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 entity polynom is
 	port (
@@ -26,14 +27,29 @@ architecture behav of polynom is
 
 	type poly_coeff_type is array(0 to POLY_DEGREE) of signed(8 downto 0);
 
-	constant POLY_COEFF : poly_coeff_type := (
-			0 => to_signed(-8, POLY_COEFF(0)'length),
-			1 => to_signed(-3, POLY_COEFF(0)'length),
-			2 => to_signed(4, POLY_COEFF(0)'length),
-			3 => to_signed(-2, POLY_COEFF(0)'length),
-			4 => to_signed(1, POLY_COEFF(0)'length)
-		);
+--	constant POLY_COEFF : poly_coeff_type := (
+--			0 => to_signed(-8, POLY_COEFF(0)'length),
+--			1 => to_signed(-3, POLY_COEFF(0)'length),
+--			2 => to_signed(4, POLY_COEFF(0)'length),
+--			3 => to_signed(-2, POLY_COEFF(0)'length),
+--			4 => to_signed(1, POLY_COEFF(0)'length)
+--		);
+		
+	--constant POLY_COEFF : poly_coeff_type := (-8, -3, 4, -2, 1);
 
 	-- TODO: Add your Implementation here
-	fx <= POLY_COEFF(4) * x**4 - POLY_COEFF(3) * x**3 + POLY_COEFF(2) * x**2 - POLY_COEFF(1) * x + POLY_COEFF(0);
+	begin
+	
+	process(clk)
+		begin
+		if clk'event and clk='1' then
+			if rst = '1' then
+				fx	<= 0;
+			else
+				--fx <= POLY_COEFF(4) * x**4 - POLY_COEFF(3) * x**3 + POLY_COEFF(2) * x**2 - POLY_COEFF(1) * x + POLY_COEFF(0);
+				fx <= (8 * x*x*x*x) + (3 * x*x*x) + (4 * x*x) + (2 * x) + (1);
+			end if;
+		end if;
+	end process;
+	
 end architecture;
--- polynom_tb.vhd
+++ polynom_tb.vhd
@@ -10,6 +10,12 @@ end entity;
 
 architecture behav of polynom_tb is
 	
+	constant CLK_PERIOD : time := 10 ns;
+	signal clk 	: std_logic := '0';
+	signal rst	: std_logic := '0';
+	signal x 	: integer := 0;
+	signal fx	: integer := 0;
+	
 	component polynom is
 		port (
 			-- synchronous interface
@@ -18,24 +24,54 @@ architecture behav of polynom_tb is
 
 			-- TODO: place your modules Interface here
 			x	: in integer;
-			fx	: out integer;
+			fx	: out integer
 		);
 	end component;
 
 	--Polynomial Definition (same as in module)
-	constant POLY_DEGREE : integer := 4; -- degree of polynomial
+--	constant POLY_DEGREE : integer := 4; -- degree of polynomial
+--
+--	type poly_coeff_type is array(0 to POLY_DEGREE) of signed(8 downto 0);
 
-	type poly_coeff_type is array(0 to POLY_DEGREE) of signed(8 downto 0);
-
-	constant POLY_COEFF : poly_coeff_type := (
-			0 => to_signed(-8, POLY_COEFF(0)'length),
-			1 => to_signed(-3, POLY_COEFF(0)'length),
-			2 => to_signed(4, POLY_COEFF(0)'length),
-			3 => to_signed(-2, POLY_COEFF(0)'length),
-			4 => to_signed(1, POLY_COEFF(0)'length)
-		);
+--	constant POLY_COEFF : poly_coeff_type := (
+--			0 => to_signed(-8, POLY_COEFF(0)'length),
+--			1 => to_signed(-3, POLY_COEFF(0)'length),
+--			2 => to_signed(4, POLY_COEFF(0)'length),
+--			3 => to_signed(-2, POLY_COEFF(0)'length),
+--			4 => to_signed(1, POLY_COEFF(0)'length)
+--		);
 
 	-- TODO: Implement your Testbench here
-	x <= 1;
+	begin
+
+	dut: polynom port map(
+			clk=>clk,
+			rst=>rst,
+			x=>x,
+			fx=>fx
+	);	
+	
+	clk_gen: process
+	begin
+		clk	<= not clk;
+		wait for CLK_PERIOD/2;
+	end process clk_gen;
+	
+	stimuli: process
+	begin
+		rst	<= '0';
+		wait for CLK_PERIOD;
+		rst	<= '1';
+		wait for CLK_PERIOD;
+		rst	<= '0';
+		wait for CLK_PERIOD;
+		x <= 1;
+		wait for CLK_PERIOD;
+		x <= 2;
+		wait for CLK_PERIOD;
+		x <= 3;
+		wait for 20000*CLK_PERIOD;
+		assert false report "Simulation terminated!" severity failure;
+	end process stimuli;
 	
 end;
